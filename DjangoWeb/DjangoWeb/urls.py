e58c85cc9ad00f5348a3ff551dd3dd5490f4a204
Hary Prabowo <h.prabowo@allegrodevelopment.com>
2020-05-17 07:10:15 +0700

    Authenticate users

--- DjangoWeb/DjangoWeb/urls.py
+++ DjangoWeb/DjangoWeb/urls.py
@@ -3,28 +3,31 @@ Definition of urls for DjangoWeb.
 """
 
 from datetime import datetime
+from django.conf.urls import url
 from django.urls import path
-from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
 
+from django.conf.urls import include
+from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = [
-    path('', views.home, name='home'),
+urlpatterns = [path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('login/',
-         LoginView.as_view
-         (
-             template_name='app/login.html',
+         LoginView.as_view(template_name='app/login.html',
              authentication_form=forms.BootstrapAuthenticationForm,
              extra_context=
              {
                  'title': 'Log in',
                  'year' : datetime.now().year,
-             }
-         ),
+             }),
          name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/',
+        LoginView.as_view(template_name='app/loggedoff.html'),
+        name='logout'),
     path('admin/', admin.site.urls),
-]
+
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', admin.site.urls),]
